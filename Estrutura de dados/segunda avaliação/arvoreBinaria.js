ec49e25d660de6d4756f1aafd5c9e8d6b63d8f8b
juniorferreira23 <junior.ferreira@outlook.com>
2023-11-30 21:22:03 -0300

    ajustes ate agora

--- Estrutura de dados/segunda avaliação/arvoreBinaria.js
+++ Estrutura de dados/segunda avaliação/arvoreBinaria.js
@@ -4,66 +4,63 @@
 // mas diferente das listas e pilhas que são estruturas lineares, a árvore possuem uma maneira mais sofisticada
 // e hierarquica de armazenar dados.
 
-// A estrutura base da árvore é a raiz que é a parte mais alta da árvore e possuem arestas ou nós
+// A estrutura base da árvore é a raiz que é a parte mais alta da árvore e possuem arestas
 // Então iniciaremos criando uma class Nó que terá a raiz e as arestas 
+
 class No {
-    constructor(chave){
-        // Aqui podemos observar que ao ser instânciada a classe Nó iremos atribuir o valor da chave a raiz
-        this.chave = chave
+    constructor(dado){
+        // Aqui podemos observar que ao ser instânciada a classe Nó iremos atribuir o valor do dado ao Nó
+        this.dado = dado
         // Podemos observa que os nós filhos serão a esquerda e a direita e iniciaram vázios
         this.esquerda = null
         this.direita = null
     }
 }
 
-// Ao instânciar a classe Nó atribuimos o valor da chave que irá ser a primeira raiz da classe
-const raiz = new No(1)
-// Logo em seguida intânciamos novos nós no qual informamos que além dos atributos esquerda e direta serem
-// Nós filhos eles são também raizes se suas próprias mini-árvores ou Nós pai de seus nós filhos, no qual se tivermos várias ramificações
-// passamos a ter o que é chamado de floresta, no qual seria vários nós com suas respectivas "raizes".
-raiz.esquerda = new No(2)
-raiz.direita = new No(3)
+// Ao instânciar a classe Nó atribuimos o valor do dado que irá ser a primeira raiz da classe
+
+// const raiz = new No(1)
+
+// Logo em seguida intânciamos novos nós no qual informamos que os atributos esquerda e direta serão Nós filhos
+// Em caso de haver vários Nós nãos conectados são chamados de florestas.
+
+// raiz.esquerda = new No(2)
+// raiz.direita = new No(3)
+
 // Observamos que o código abaixo demostra como cada ramificação tem seus atributos a esquerda e a direita,
 // assim dando o nome desse modelo de estrutura de Árvore binária, pois só possui duas ramificações.
-raiz.esquerda.esquerda = new No(4)
-raiz.esquerda.direita = new No(5)
-raiz.direita.esquerda = new No(6)
-raiz.direita.esquerda.esquerda = new No(7)
+
+// raiz.esquerda.esquerda = new No(4)
+// raiz.esquerda.direita = new No(5)
+// raiz.direita.esquerda = new No(6)
+// raiz.direita.esquerda.esquerda = new No(7)
+
 // Por fim os últimos Nós e os mais profundos são chamados de folhas
+
 //console.log(raiz)
-//console.log(raiz.direita.esquerda.esquerda) // Nó mais profundo
+
 
 //Exemplificação de vamos das chaves sendo "v" o ponteiro para baixo informado que Nó a esquerda ou a direita
-//   v      1      v        1º Camada de profundidade
-// v 2 v         v 3        2º Camada de profundidade
-// 4   5       v 6          3º Camada de profundidade
-//           v 7            4º Camada de profundidade
+//               1               1º Camada de profundidade
+//          /         \
+//         2          3          2º Camada de profundidade
+//        / \        / \
+//      4   5       6            3º Camada de profundidade
+//     /\   /\     /\
+//           7                   4º Camada de profundidade
 
-// Efetuando uma busca na árvore binária
-const calcularProfundidade = (no, contador) => {
-    let contadorEsquerda = contador
-    let contadorDireita = contador
-    // Verificando se o nó não está vázio/nulo
-    if (no === null) {
-        //console.log('Nó vázio');
-        return 0
-    } else {
-        // Recursivamente calcula a profundidade de cada subárvore
-        //console.log(no.esquerda)
-        
-        contadorEsquerda++
-        const profundidadeEsquerda = calcularProfundidade(no.esquerda, contadorEsquerda) //chamando a função novamente com o atributo a esquerda do no
-        console.log(profundidadeEsquerda)
-        
-        contadorDireita++
-        const profundidadeDireita = calcularProfundidade(no.direita, contadorDireita) //idm
-        console.log(profundidadeDireita)
-        
-        
-        // A profundidade da árvore é a maior profundidade entre as subárvores mais a raiz
-        //console.log('profundidade é: ', Math.max(profundidadeEsquerda, profundidadeDireita) + 1)
-        console.log(contadorDireita,contadorEsquerda)
+// Evoluindo nosso código podemos separar a classe nó e a classe árvore binária que irá possuir o atributo raiz e instaciamos ela.
+class ArvoreBinaria {
+    constructor(data){
+        let no = new No(data)
+        this.raiz  = no
     }
 }
 
-calcularProfundidade(raiz, 1)
+const arvore = new ArvoreBinaria(7)
+arvore.esquerda = new No(18)
+arvore.direita = new No(14)
+
+console.log(arvore.raiz)
+console.log(arvore.esquerda)
+console.log(arvore.direita)
